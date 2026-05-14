cbba4aea669f22282e684620ff7c6d0bd0cb6845
K Akshitha <akshithasharma682@gmail.com>
2026-05-14 13:21:16 +0530

    Delete Week-2 directory

--- Week-2/w2assgn14.js
+++
@@ -1,9 +0,0 @@
-//exam portal simulator
-
-console.log("Exam Submitted successfully");
-setTimeout(()=>{
-    console.log("Evaluatig answers...")
-},2000)
-setTimeout(()=>{
-    console.log("Result: Pass")
-},4000)
--- Week-2/w2assgn6.js/app.js
+++
@@ -1,9 +0,0 @@
-import { addTask,getAllTasks,completeTask } from "./task.js"; 
-
-addTask('read','high','23-03-2026')
-addTask('write','medium','23-03-2026')
-addTask('eat','high','23-03-2026')
-
-getAllTasks();
-
-completeTask('write');
