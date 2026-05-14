292322d8695a3d1cc8e27785030ee42682ac6f6b
K Akshitha <akshithasharma682@gmail.com>
2026-05-14 13:20:59 +0530

    Delete Week-1 directory

--- Week-1/w1prog3.js
+++
@@ -1,9 +0,0 @@
-//sum of elements of an array
-
-let marks=[90,78,65,98];
-let sum=0;
-console.log("marks :",marks)
-for(let i=0;i<marks.length;i++){
-    sum+=marks[i];
-}
-console.log("total :",sum);
